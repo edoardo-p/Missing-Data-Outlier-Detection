16bd840128082f15dce82c3123a1ab9543257d6d
edoardo-p <ep742@bath.ac.uk>
2023-01-16 12:38:12 +0100

    Added BIC optimization

--- cc_clustering.R
+++ cc_clustering.R
@@ -6,8 +6,16 @@ cc_missing <- ampute(cc, 0.05)
 sapply(cc_missing, function(x) sum(is.na(x)))
 
 cc_missing <- cc_missing[-1]
-# mtm_model <- MtM(cc_missing, 2)
-mnm_model <- MNM(cc_missing, 2, max_iter = 1)
+# mtm_model <- MtM(cc_missing, 2)+
+best_bic <- 9999999999
+best_g <- 0
+for (G in 2:5) {
+    mnm_model <- MNM(cc_missing, G, max_iter = 1)
+    if (mnm_model$BIC < best_bic) {
+        best_bic <- mnm_model$BIC
+        best_g <- G
+    }
+}
 # mcnm_model <- MCNM(cc_missing, 2)
 
 ggplot(cc_missing, aes(x = `BALANCE`, y = `PAYMENTS`)) +
