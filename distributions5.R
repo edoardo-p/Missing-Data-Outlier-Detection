ad409f22d00f81a5701bdf7a32c0b9402746495b
edoardo-p <ep742@bath.ac.uk>
2023-01-16 11:57:04 +0100

    Delete distributions5

--- distributions5.R
+++
@@ -1,6 +0,0 @@
-library(clusterGeneration)
-library(dplyr)
-
-mn_mix <- genRandomClust(5, 0.8, numNonNoisy = 10, numReplicate = 1, clustszind = 3, clustSizes = c(100, 150, 200, 200, 350))
-dataset <- as.data.frame(mn_mix$datList)
-mn_mix_standard <- dataset %>% mutate_all(~ (scale(.) %>% as.vector()))
