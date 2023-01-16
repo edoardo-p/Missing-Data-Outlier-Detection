f554923b31952af5610a3c59383b4b7d9d199da7
edoardo-p <ep742@bath.ac.uk>
2023-01-16 12:32:40 +0100

    Fixed ggplot

--- cc_clustering.R
+++ cc_clustering.R
@@ -7,10 +7,10 @@ sapply(cc_missing, function(x) sum(is.na(x)))
 
 cc_missing <- cc_missing[-1]
 # mtm_model <- MtM(cc_missing, 2)
-mnm_model <- MNM(cc_missing, 2)
+mnm_model <- MNM(cc_missing, 2, max_iter = 1)
 # mcnm_model <- MCNM(cc_missing, 2)
 
-ggplot(cc_missing, aes(x = `BALANCE`, y = `BALANCE_FREQUENCY`)) +
+ggplot(cc_missing, aes(x = `BALANCE`, y = `PAYMENTS`)) +
     geom_point(aes(colour = factor(mnm_model$clusters))) +
     scale_color_manual(values = c("green", "blue")) +
-    ggtitle(title)
+    ggtitle("Results")
