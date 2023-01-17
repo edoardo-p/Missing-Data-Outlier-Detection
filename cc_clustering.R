8a02312b72d77c6a2e4c7353424f1956bad8f871
edoardo-p <ep742@bath.ac.uk>
2023-01-17 13:10:58 +0100

    Refactored data preprocessing

--- cc_clustering.R
+++ cc_clustering.R
@@ -1,6 +1,4 @@
 library(MixtureMissing)
-library(matrixcalc)
-library(tidyverse)
 cat("\014")
 rm(list = ls())
 
@@ -17,19 +15,18 @@ ampute <- function(data, prop) {
 # Data preprocessing
 music_data <- read.csv("music_genre.csv")
 
-# Remove categorical variables and the label
-index <- c(1, 2, 3, 10, 13, 16, 18)
+# Remove all rows with missing values
+music_data$tempo[which(music_data$tempo == "?")] <- NA
+music_data$tempo <- as.double(music_data$tempo)
+music_data$duration_ms[which(music_data$duration_ms == -1)] <- NA
+music_data <- music_data[-which(is.na(music_data$duration_ms)), ]
+music_data <- music_data[-which(is.na(music_data$tempo)), ]
 
 # Save labeled genre for training the model
 label <- music_data[18]
-music_data <- music_data[-index]
 
-# Substitute '?' with NA values and convert column to double
-music_data[which(music_data$tempo == "?"), ] <- NA
-music_data$tempo <- as.double(music_data$tempo)
-
-# Remove empty rows
-music_data <- music_data[-which(is.na(music_data$popularity)), ]
+# Remove categorical variables and the label
+music_data <- music_data[-c(1, 2, 3, 10, 13, 16, 18)]
 
 # Ampute dataset to randomly insert NAs
 music_missing <- ampute(music_data, 0.01)
@@ -39,30 +36,36 @@ sample <- sample(1:nrow(music_missing), nrow(music_missing) * 0.8)
 train <- music_missing[sample, ]
 test <- music_missing[-sample, ]
 
-best_bic <- 0
-best_g <- 0
-cc_missing <- music_missing[sample(1:nrow(music_missing), 500), ]
-cc_missing$duration_ms[which(cc_missing$duration_ms == -1)] <- NA
-cc_missing <- cc_missing[, -2]
+
+sub <- music_data[which(label == "Rock" | label == "Blues" | label == "Rap"), ]
+
+# sub <- sub[, -2]
 best_bic_list <- list()
-for (G in 6:15) {
-    mnm_model <- MNM(cc_missing, G, max_iter = 10, identity_cov = TRUE)
-    best_bic <- mnm_model$BIC
-    best_bic_list <- append(best_bic_list, best_bic)
-    best_g <- G
+for (G in 2:5) {
+    mnm_model <- MNM(sub, G, max_iter = 20, identity_cov = TRUE)
+    best_bic_list <- append(best_bic_list, mnm_model$BIC)
 }
+
+
+
+
+
+
+
+
+
+
+
 
 # Plot BIC
 plot(as.numeric(best_bic_list), type = "b", pch = 20, col = "orange")
 grid(nx = NULL, ny = NULL, lty = 2, col = "black", lwd = 2)
 
-mnm_model <- MNM(cc_missing, 3, max_iter = 1)
+mnm_model <- MNM(music_missing, 3, max_iter = 1)
 
-ggplot(cc_missing, aes(x = `BALANCE`, y = `PURCHASE`)) +
+ggplot(music_missing, aes(x = `BALANCE`, y = `PURCHASE`)) +
     geom_point(aes(colour = factor(mnm_model$clusters))) +
     scale_color_manual(values = c("green", "blue")) +
     ggtitle("results")
-attach(cc_missing)
+attach(music_missing)
 mnm_model$clusters
-df_new <- cbind(BALANCE, PURCHASES, CASH_ADVANCE, CREDIT_LIMIT, PAYMENTS, PRC_FULL_PAYMENT, mnm_model$clusters)
-pairs(df_new[sample(1:nrow(df_new), nrow(df_new) * 0.1), ], col = c("red", "blue"))
